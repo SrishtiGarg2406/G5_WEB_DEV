1ee2224860a1bef10fc6e5fd515502bdad81fdd2
SrishtiGarg2406 <123459717+SrishtiGarg2406@users.noreply.github.com>
2025-07-02 10:02:21 +0530

    ..

---
+++ L1_01_07/script.js
@@ -0,0 +1,37 @@
+// console.log("HELLO WORLD")
+// var a=200;
+// console.log(a);
+// function abc(){
+//     console.log("HELLO CLASS");
+// }
+// var b=3000;
+
+// console.log(b);
+// abc();
+// // setTimeout(()=>{
+// //     console.log("I am set time out")
+// // },2000);
+// console.log("End of program")
+
+function grandparent(){
+    var a=200;
+    console.log(a);
+    //console.log(c);
+
+    function parent(){
+        var b=300;
+        console.log(b);
+
+        function child(){
+            var c=400;
+            var a=9000;
+            console.log(c);
+            console.log(a);
+        }
+        child();
+    }
+    parent();
+}
+grandparent();
+
+
