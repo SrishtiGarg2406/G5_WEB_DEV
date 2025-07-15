8fc72f1eee593ea71bfd534410d0b6538c3a8ad5
SrishtiGarg2406 <123459717+SrishtiGarg2406@users.noreply.github.com>
2025-07-15 22:47:41 +0530

    .

---
+++ l9_15_07/index.js
@@ -0,0 +1,18 @@
+// const sumOfTwo =require("./sumOfTwo");
+// const {divOfTwo,xy}=require("./divOfTwo");
+// // console.log("Master");
+
+
+
+// // function subOfTwo(a,b){
+// //     console.log(a-b);
+// //     return a-b;
+// // }
+
+// divOfTwo(20,2);
+// // subOfTwo(30,40);
+// sumOfTwo(10,5);
+// console.log(xy);
+
+
+
